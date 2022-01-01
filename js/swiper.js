09c8de6a858baaabd31d096458ab985d89e2b861
Seungyeol <marudev4905@gmail.com>
2022-01-01 20:59:58 +0900

    '.'

--- js/swiper.js
+++ js/swiper.js
@@ -1,12 +1,17 @@
 var swiper = new Swiper('.swiper', {
-  loop: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  spaceBetween: 25,
-  slideToClickedSlide: true,
-  effect: 'coverflow',
-  coverflowEffect: {
-    rotate: 0,
-    slideShadows: false,
-  },
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    slideToClickedSlide: true,
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 0,
+        slideShadows: false,
+    },
+    breakpoints: {
+      768: {
+        spaceBetween: 25,
+      },
+    },
 });
